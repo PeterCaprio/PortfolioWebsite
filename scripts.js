f773c1841240a8325a27af1ccbc502915df98ea8
Peter Caprio <petercaprio@Peters-MacBook-Pro-2.local>
2023-10-17 19:10:47 -0600

    renamed to index

--- scripts.js
+++ scripts.js
@@ -1,18 +1,38 @@
-function openTab(tabId, element) {
-    var contentEls = document.getElementsByClassName("tab-content");
-    for (var i = 0; i < contentEls.length; i++) {
-        contentEls[i].style.display = "none";
-    }
+document.addEventListener("DOMContentLoaded", function() {
+    showTab('coding'); // Default tab to show
 
-    var tabLinks = document.getElementsByClassName("tab-link");
-    for (var i = 0; i < tabLinks.length; i++) {
-        tabLinks[i].classList.remove("active");
-    }
+    // Initialize Swiper carousel
+    var mySwiper = new Swiper('.swiper-container', {
+        lazy: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+});
 
-    document.getElementById(tabId).style.display = "block";
-    element.classList.add("active");
+function showTab(tabId) {
+    var contents = document.querySelectorAll('.tab-content');
+    var buttons = document.querySelectorAll('.tab-button');
+
+    contents.forEach(function(content) {
+        content.style.display = 'none';
+    });
+
+    buttons.forEach(function(button) {
+        button.classList.remove('active');
+    });
+
+    document.getElementById(tabId).style.display = 'block';
+    document.querySelector(`.tab-button[onclick="showTab('${tabId}')"]`).classList.add('active');
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("defaultTab").click();
-});
+function hoverButton(button) {
+    button.style.backgroundColor = "#ccc";
+}
+
+function unhoverButton(button) {
+    if (!button.classList.contains('active')) {
+        button.style.backgroundColor = "";
+    }
+}
